4348f8a17d9ac87c29effe0bcf8a1ef362111ce4
micap <micap@mba>
2014-03-30 21:29:02 +1100

    Added CountryPickerViewController tableView datasource implementations

--- Classes/ios/CPCountryPickerViewController.m
+++ Classes/ios/CPCountryPickerViewController.m
@@ -7,19 +7,29 @@
 //
 
 #import "CPCountryPickerViewController.h"
+#import "CPChunkList.h"
+#import "CPCountryCallingCodes.h"
+
+static NSString *const kCellIdentifier = @"CountryCell";
 
 @interface CPCountryPickerViewController ()
-
+@property (nonatomic, strong) NSDictionary *countries;
+@property (nonatomic, strong) NSArray *sectionHeader;
 @end
 
 @implementation CPCountryPickerViewController
-
 - (id)initWithStyle:(UITableViewStyle)style
 {
-    self = [super initWithStyle:style];
+    return [self initWithCountryCodeList:[CPCountryCallingCodes countryCallingCodes]];
+}
+
+- (id)initWithCountryCodeList:(NSArray *)countryCodes
+{
+    self = [super initWithStyle:UITableViewStylePlain];
     if (self) {
-        // Custom initialization
+        _countries = [CPChunkList chunklist:countryCodes];
     }
+    
     return self;
 }
 
@@ -28,6 +38,9 @@
     [super viewDidLoad];
     
     [self setDefaultTitle];
+    self.sectionHeader = [self.countries allKeys];
+    
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:kCellIdentifier];
 }
 
 - (void)didReceiveMemoryWarning
@@ -46,28 +59,26 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 0;
+    return self.sectionHeader.count;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 0;
+    NSString *key = [self.sectionHeader objectAtIndex:section];
+    return [self.countries[key] count];
 }
 
-/*
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kCellIdentifier forIndexPath:indexPath];
     
-    // Configure the cell...
+    NSString *key = [self.sectionHeader objectAtIndex:indexPath.section];
+    
+    cell.textLabel.text = self.countries[key][indexPath.row];
     
     return cell;
 }
-*/
+
 
 /*
 // Override to support conditional editing of the table view.
--- Tests/CallingPidgeonTests/CallingPidgeonTests/CPCountryPickerViewControllerSpec.m
+++ Tests/CallingPidgeonTests/CallingPidgeonTests/CPCountryPickerViewControllerSpec.m
@@ -9,9 +9,19 @@
 #import <Kiwi/Kiwi.h>
 #import <CallingPidgeon/CPCountryPickerViewController.h>
 
+@interface CPCountryPickerViewController(Tests)
+- (id)initWithCountryCodeList:(NSArray *)countryCodes;
+@end
+
+
 SPEC_BEGIN(CPCountryPickerViewControllerSpec)
 
 describe(@"CPCountryPickerViewController displays a list countries to pick from", ^{
+    beforeEach(^{
+        NSBundle *testBundle = [NSBundle bundleForClass:self];
+        [NSBundle stub:@selector(mainBundle) andReturn:testBundle];
+    });
+    
     it(@"has a default title of 'Select a country'", ^{
         CPCountryPickerViewController *viewController = [[CPCountryPickerViewController alloc] init];
         [viewController viewDidLoad];
@@ -25,6 +35,26 @@ describe(@"CPCountryPickerViewController displays a list countries to pick from"
         viewController.title = expectedTitle;
         [viewController viewDidLoad];
         [[viewController.title should] equal:expectedTitle];
+    });
+    
+    context(@"CPCountryPickerViewController as a tableView datasource", ^{
+        __block CPCountryPickerViewController *viewController;
+        beforeEach(^{
+            viewController = [[CPCountryPickerViewController alloc] initWithCountryCodeList:@[@"Australia", @"Angola", @"Bahamas"]];
+        });
+        
+        it(@"number of sections is equal to the number grouping for the country list", ^{
+            [[@([viewController numberOfSectionsInTableView:viewController.tableView]) should] equal:@(2)];
+        });
+        
+        it(@"number of rows is equal to the number countries for that section", ^{
+            [[@([viewController tableView:viewController.tableView numberOfRowsInSection:0]) should] equal:@(2)];
+            [[@([viewController tableView:viewController.tableView numberOfRowsInSection:1]) should] equal:@(1)];
+        });
+        it(@"configures a UITableViewCell with the country and its calling code", ^{
+            UITableViewCell *cell = [viewController tableView:viewController.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]];
+            [[cell.textLabel.text should] equal:@"Australia"];
+        });
     });
 });
 SPEC_END
